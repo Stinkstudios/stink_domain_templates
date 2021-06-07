7f69598c1bb92e24d96e7de32b75d4d10d12679a
Ben Priddy <colorandsound@gmail.com>
2021-06-07 17:29:50 -0400

    modify base deskStructure. add copyDeck to lib schema

--- add/sanity/cms/config/deskStructure.js
+++ add/sanity/cms/config/deskStructure.js
@@ -2,31 +2,34 @@ import S from '@sanity/desk-tool/structure-builder'
 
 import { VscGear } from 'react-icons/vsc'
 import { RiPagesLine } from 'react-icons/ri'
-
-const generateDoc = (type, title, icon, id) => {
-	const doc = S.listItem()
-		.title(title)
-		.child(
-			S.document()
+import { FaSuitcase } from 'react-icons/fa'
+		
+		const generateDoc = ({type, title, icon, id}) => {
+			const doc = S.listItem()
 				.title(title)
-				.schemaType(type)
-				.documentId(id || type)
-		)
-	return icon ? doc.icon(icon) : icon === false ? doc.showIcon(false) : doc
-}
-
-const generatePagesList = () => {
-	return S.listItem()
-		.title('Pages')
-		.icon(RiPagesLine)
-		.child(S.documentList().filter(`_type == 'page'`).title('Pages'))
-}
-
-export default () =>
-	S.list()
-		.title('Boilerplate desk')
-		.items([
-			generateDoc('singletonSiteConfiguration', 'Site Configuration', VscGear),
-			S.divider(),
-			generatePagesList()
-		])
+				.child(
+					S.document()
+						.title(title)
+						.schemaType(type)
+						.documentId(id || type)
+				)
+			return icon ? doc.icon(icon) : icon === false ? doc.showIcon(false) : doc
+		}
+		
+		const generateDocumentsList = ({title, type, icon=RiPagesLine}) => {
+			return S.listItem()
+				.title(title)
+				.icon(RiPagesLine)
+				.child(S.documentList().filter(`_type == '${type}'`).title(title))
+		}
+		
+		export default () =>
+			S.list()
+				.title('Boilerplate desk')
+				.items([
+					generateDoc({type:'singletonSiteConfiguration', title:'Site Configuration', icon:VscGear}),
+					generateDoc({type:'copyDeck', title:'Site Copy', icon:FaSuitcase}),
+					S.divider(),
+					generateDocumentsList({title:'Pages', type:'page', icon:RiPagesLine}),
+				])
+		 
